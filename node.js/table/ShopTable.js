61752dc14a2246896d284e621a61dc023593e154
ChayaStraus <chaya280905@gmail.com>
2025-03-08 21:03:27 +0200

    including node.js

---
+++ node.js/table/ShopTable.js
@@ -0,0 +1,23 @@
+import mongoose, { model, Schema } from 'mongoose'
+
+const ShopTable = Schema({
+    codeCustomer: {
+        type: mongoose.Types.ObjectId,
+        ref: 'customers'
+    },
+    date: String,
+    gameArr: [
+        {
+            codeGame: {
+                type: mongoose.Types.ObjectId,
+                ref: 'Games'
+            },
+            name: String,
+            price: Number,
+            amount: Number,
+            totalprice: Number
+        }
+    ],
+    sum: Number,
+})
+export default model("Shops",ShopTable)
